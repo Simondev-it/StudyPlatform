3876908ac4182f454d8ebef5f2fbf031df1afb41
NhatMinh <mcpegunny@gmail.com>
2025-06-14 23:01:55 +0700

    xương máu, lỗi từ 12 tới nay mới đc

---
+++ Repository/Repositories/ChapterRepository.cs
@@ -0,0 +1,25 @@
+﻿ using Repository.Interfaces;
+using StudyPlatform.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Repository.Repositories
+{
+    public class ChapterRepository : GenericRepository<Chapter>, IChapterRepository
+    {
+        private readonly StudyPlatformContext _context;
+
+        public ChapterRepository(StudyPlatformContext context) : base(context)
+        {
+            _context = context;
+        }
+
+         
+
+         
+    }
+}
---
+++ Service/Interfaces/IChapterService.cs
@@ -0,0 +1,14 @@
+﻿ using StudyPlatform.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Service.Interfaces
+{
+    public interface IChapterService
+    {
+        Task<List<Chapter>> GetAllChaptersAsync();
+    }
+}
