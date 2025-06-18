d14c8a9afeaf3fd412ae6d16502a74dd3d0eb8c6
NhatMinh <mcpegunny@gmail.com>
2025-06-18 23:38:04 +0700

    add unit of work

---
+++ Repository/Interfaces/IUnitOfWork.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Repository.Interfaces
+{
+    public interface IUnitOfWork : IDisposable
+    {
+        public IChapterRepository ChapterRepository { get; }
+        Task<int> SaveChangeAsync();
+
+
+    }
+}
